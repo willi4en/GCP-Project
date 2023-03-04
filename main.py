6bfdef453deec9063a5e60e1abd2f6efc6a9b47c
willi4en <willi4en@mail.uc.edu>
2023-03-04 14:52:00 -0500

    Intro

--- main.py
+++ main.py
@@ -5,9 +5,35 @@ app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    if request.method == 'POST':
+        firstName = request.form['fname']
+        lastName = request.form['lname']
+        email = request.form['email']
+        return redirect(url_for('chat', firstName=firstName, lastName=lastName, email=email, answer=""))
+    elif request.method == 'GET':
+        return render_template('home.html')
+
+
+@app.route('/chat', methods=['GET', 'POST'])
+def chat(firstName, lastName, email, answer):
+    if request.method == 'POST':
+        if 'q1' in request.form:
+            return render_template('chat.html', firstName=firstName, lastName=lastName, email=email, answer="Q1 Answer")
+        elif 'q2' in request.form:
+            return render_template('chat.html', firstName=firstName, lastName=lastName, email=email, answer="Q2 Answer")
+        elif 'q3' in request.form:
+            return render_template('chat.html', firstName=firstName, lastName=lastName, email=email, answer="Q3 Answer")
+        elif 'q4' in request.form:
+            return render_template('chat.html', firstName=firstName, lastName=lastName, email=email, answer="Q4 Answer")
+        elif 'end' in request.form:
+            return redirect(url_for('end', firstName=firstName, lastName=lastName, email=email))
+        else:
+            return render_template('chat.html', firstName="Error", lastName="Error", email="Error", answer="Error")
+
+    elif request.method == 'GET':
+        return render_template('chat.html', firstName=firstName, lastName=lastName, email=email, answer="Start")
 
 
 if __name__ == '__main__':
