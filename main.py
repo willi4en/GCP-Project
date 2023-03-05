33f5e3923bc98a43bd413895998ab7e57dfd7bc0
willi4en <willi4en@mail.uc.edu>
2023-03-05 10:04:33 -0500

    Finished

--- main.py
+++ main.py
@@ -8,33 +8,38 @@ app.secret_key = os.urandom(24)
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        firstName = request.form['fname']
-        lastName = request.form['lname']
-        email = request.form['email']
-        return redirect(url_for('chat', firstName=firstName, lastName=lastName, email=email, answer=""))
+        session['firstName'] = request.form['fname']
+        session['lastName'] = request.form['lname']
+        session['email'] = request.form['email']
+        return redirect(url_for('chat'))
     elif request.method == 'GET':
         return render_template('home.html')
 
 
 @app.route('/chat', methods=['GET', 'POST'])
-def chat(firstName, lastName, email, answer):
+def chat():
     if request.method == 'POST':
-        if 'q1' in request.form:
-            return render_template('chat.html', firstName=firstName, lastName=lastName, email=email, answer="Q1 Answer")
-        elif 'q2' in request.form:
-            return render_template('chat.html', firstName=firstName, lastName=lastName, email=email, answer="Q2 Answer")
-        elif 'q3' in request.form:
-            return render_template('chat.html', firstName=firstName, lastName=lastName, email=email, answer="Q3 Answer")
-        elif 'q4' in request.form:
-            return render_template('chat.html', firstName=firstName, lastName=lastName, email=email, answer="Q4 Answer")
-        elif 'end' in request.form:
-            return redirect(url_for('end', firstName=firstName, lastName=lastName, email=email))
+        if request.form['q'] == "Does UC have a football team?":
+            return render_template('chat.html', firstName=session['firstName'], lastName=session['lastName'], email=session['email'], answer="Yes, the Bearcats.")
+        elif request.form['q'] == "Does UC have Computer Science Major?":
+            return render_template('chat.html', firstName=session['firstName'], lastName=session['lastName'], email=session['email'], answer="UC does have a computer science major.")
+        elif request.form['q'] == "Does UC have on campus housing?":
+            return render_template('chat.html', firstName=session['firstName'], lastName=session['lastName'], email=session['email'], answer="They do have on campus housing.")
+        elif request.form['q'] == "Do the dining halls have good food?":
+            return render_template('chat.html', firstName=session['firstName'], lastName=session['lastName'], email=session['email'], answer="No...they do not.")
+        elif request.form['q'] == "End":
+            return redirect(url_for('end'))
         else:
             return render_template('chat.html', firstName="Error", lastName="Error", email="Error", answer="Error")
 
     elif request.method == 'GET':
-        return render_template('chat.html', firstName=firstName, lastName=lastName, email=email, answer="Start")
+        return render_template('chat.html', firstName=session['firstName'], lastName=session['lastName'], email=session['email'], answer="Start")
+
+
+@app.route('/end', methods=['GET', 'POST'])
+def end():
+    return render_template('end.html', firstName=session['firstName'], lastName=session['lastName'], email=session['email'])
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=80)
